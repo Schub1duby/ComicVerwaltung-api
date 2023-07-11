7f2bb86aa0970173336a2e3a56e3c039eb84f697
cyrillschudel <cyrill.schudel@bluewin.ch>
2023-07-11 08:26:06 +0200

    initial commit

---
+++ src/main/java/ch/raiffeisen/ComicVerwaltungapi/ComicVerwaltungApiApplication.java
@@ -0,0 +1,13 @@
+package ch.raiffeisen.ComicVerwaltungapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicVerwaltungApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicVerwaltungApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/raiffeisen/ComicVerwaltungapi/ComicVerwaltungApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.raiffeisen.ComicVerwaltungapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicVerwaltungApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
